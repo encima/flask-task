f36bc81d1e967c814dbae10dfc0664926438cb13
encima <encima@gmail.com>
2016-06-09 21:15:58 +0100

    fixed regex for urgency and tags

--- app/test_tw.py
+++ app/test_tw.py
@@ -9,11 +9,12 @@ class Test_TW(unittest.TestCase):
         self.twl = TW_Loader()
 
     def test_add(self):
-        parsed_task = self.twl.add_task("task:hello there project:test_project +tag +tags")
+        parsed_task = self.twl.add_task("task:hello there project:test_project +tag +tags urgency:h")
         print(parsed_task)
         self.assertEqual(parsed_task['task'], 'hello there')
         self.assertEqual(parsed_task['project'], 'test_project')
         self.assertEqual(parsed_task['tags'], ['tag', 'tags'])
+        self.assertEqual(parsed_task['urgency'], 'H')
 
 if __name__ == '__main__':
     unittest.main()
