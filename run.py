2655aecf9b583a051334843e18fa9860632d9c60
encima <encima@gmail.com>
2016-11-23 14:21:47 +0000

    port changes and production fixes

--- run.py
+++ run.py
@@ -7,18 +7,18 @@ from passlib.hash import sha256_crypt
 
 if app.config['ENV'] == 'PROD':
     db.create_all()
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)
-    print("Running production")
-    IOLoop.instance().start()
-elif app.config['ENV'] == 'DEV':
-    db.drop_all()
-    db.create_all()
     email = app.config['ADMIN_EMAIL']
     pw = app.config['ADMIN_PW']
     hash = sha256_crypt.encrypt(pw)
     user = models.User(email, hash)
     db.session.add(user)
     db.session.commit()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(9000)
+    print("Running production on 9000")
+    IOLoop.instance().start()
+elif app.config['ENV'] == 'DEV':
+    db.drop_all()
+    db.create_all()
     print("Running development")
     app.run(debug=True)
