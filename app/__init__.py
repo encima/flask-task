d07dfd7c6f92dc56ba0eee71aa81e4508362addc
G <px9e@gmx.fr>
2019-02-09 15:21:08 +0100

    Config: Add a config helper.
    * Add a manager.py for application CLI.
    
    * Add an helper to generate config.
    
    Signed-off-by: Guillaume Lastecoueres <px9e@gmx.fr>

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+"""Application initialization."""
 import os
 from flask import Flask, request, redirect, url_for, render_template, g
 from flask_sqlalchemy import SQLAlchemy
 import flask.ext.login as flask_login
 
 app = Flask(__name__)
-app.config.from_pyfile('sample.cfg', silent=True)
+if os.path.exists("app/config/local.cfg"):
+    app.config.from_pyfile('config/local.cfg', silent=True)
+else:
+    app.config.from_pyfile('sample.cfg', silent=True)
+
 db = SQLAlchemy(app)
 
 login_manager = flask_login.LoginManager()
@@ -12,4 +17,4 @@ login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-from app import views, models
+from app import views, models  # noqa
